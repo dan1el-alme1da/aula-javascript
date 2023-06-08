f3e32ac5dc856a847d79069d2a1b579dd5f3972c
daniel almeida <daniel.segcorp@gmail.com>
2023-06-08 14:42:08 -0300

    inicio mini projeto

--- if else/aula.js
+++ if else/aula.js
@@ -1,1 +1,16 @@
 console.log('laurinha');
+
+// se a hora estiver entre 6:00 até 12:00 : bom dia
+//se a hora estiver entre 12:00 até 18:00 : boa tarde
+//senao : boa noite
+
+if (condiçao){
+    //codigo a ser executado
+}
+else if(outracondiçao){
+    //codigo a ser executado
+}
+else {
+    //codigo a ser executado
+
+}
